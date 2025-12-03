86fb53f78828a275131cf6822637cef25496c1eb
Makio64 <davronai@gmail.com>
2025-12-03 18:09:06 +0900

    shorter name in menu for the shadow demo

--- demo/src/components/ui/MenuFullscreen.vue
+++ demo/src/components/ui/MenuFullscreen.vue
@@ -48,7 +48,7 @@ export default {
 				{ text: 'Vertex Colors', href: '/examples/vertex-colors' },
 				{ text: 'GPU Circle', href: '/examples/gpu-circle' },
 				{ text: 'GPU Instancing', href: '/examples/gpu-instance' },
-				{ text: 'Sculpture Shadow', href: '/examples/shadow' },
+				{ text: 'Shadows', href: '/examples/shadow' },
 			],
 			advancedLinks: [
 				{ text: 'Sandbox', href: '/examples/sandbox', primary: true },
